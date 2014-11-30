c0af8819034e96c315e2ea5f17d8a03f0a518ed5
Slind14 <slind014@gmail.com>
2014-11-30 17:21:41 +0100

    removed log4j

--- src/main/java/me/nallar/javapatcher/PatcherLog.java
+++ src/main/java/me/nallar/javapatcher/PatcherLog.java
@@ -1,8 +1,7 @@
 package me.nallar.javapatcher;
 
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
+import java.util.logging.Logger;
 
 /**
  * For internal use only, used to Log patcher errors/warnings/info.
@@ -12,14 +11,14 @@ public class PatcherLog {
 	/*
 	 * This class might get classloaded twice under different classloaders. Don't do anything important in a static {} block.
 	 */
-	public static final Logger LOGGER = LogManager.getLogger("JavaPatcher");
+	public static final Logger LOGGER = java.util.logging.Logger.getLogger("JavaPatcher");
 
 	public static void error(String msg) {
-		LOGGER.error(msg);
+		LOGGER.severe(msg);
 	}
 
 	public static void warn(String msg) {
-		LOGGER.warn(msg);
+		LOGGER.warning(msg);
 	}
 
 	public static void info(String msg) {
@@ -27,30 +26,30 @@ public class PatcherLog {
 	}
 
 	public static void trace(String msg) {
-		LOGGER.trace(msg);
+		LOGGER.finest(msg);
 	}
 
 	public static void error(String msg, Throwable t) {
-		LOGGER.log(Level.ERROR, msg, t);
+		LOGGER.log(java.util.logging.Level.SEVERE, msg, t);
 	}
 
 	public static void warn(String msg, Throwable t) {
-		LOGGER.log(Level.WARN, msg, t);
+		LOGGER.log(java.util.logging.Level.WARNING, msg, t);
 	}
 
 	public static void info(String msg, Throwable t) {
-		LOGGER.log(Level.INFO, msg, t);
+		LOGGER.log(java.util.logging.Level.INFO, msg, t);
 	}
 
 	public static void trace(String msg, Throwable t) {
-		LOGGER.log(Level.TRACE, msg, t);
+		LOGGER.log(java.util.logging.Level.FINEST, msg, t);
 	}
 
 	public static String classString(Object o) {
 		return "c " + o.getClass().getName() + ' ';
 	}
 
-	public static void log(Level level, Throwable throwable, String s) {
+	public static void log(java.util.logging.Level level, Throwable throwable, String s) {
 		LOGGER.log(level, s, throwable);
 	}
 }
